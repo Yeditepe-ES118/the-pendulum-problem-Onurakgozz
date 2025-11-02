b808228f90c707df6be8bd55b423c36dcf7f1034
Onurakgozz <onur.akgoz@std.yeditepe.edu.tr>
2025-11-03 00:59:29 +0300

    Added assignment1.py file

---
+++ assignment1.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def find_period(L0,L1):
+    g=9.81
+    T0=2*np.pi*np.sqrt(L0/g)
+    T1=2*np.pi*np.sqrt(L1/g)
+    for L in range(L0,L1+1):
+        T=2*np.pi*np.sqrt(L/g)
+        print(f"When L = {L:2d}.0 m, T = {T:.1f} s")
+    return T0,T1
+
+L0=2
+L1=10
+T0,T1=find_period(L0,L1)
+
+print("\nReturned values:")
+print("T0=",np.round(T0,2),"s")
+print("T1=",np.round(T1,2),"s")
